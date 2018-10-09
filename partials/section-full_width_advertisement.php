768be3a3b06acf6053f86e960492e1f8c6c1f104
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-10-09 14:37:08 -0500

    Updated size of full width banner

--- partials/section-full_width_advertisement.php
+++ partials/section-full_width_advertisement.php
@@ -3,8 +3,10 @@ $ad = get_sub_field('ad');
 ?>
 
 <section class="advertisement">
-	<a href="<?php echo get_field('url',$ad->ID); ?>" target="_blank">
-		<img src="<?php echo get_field('desktop_image',$ad->ID); ?>" class="hide-mobile" style="width: 100%;">
-		<img src="<?php echo get_field('mobile_image',$ad->ID); ?>" class="show-mobile" style="width: 100%;">
-	</a>
+	<div class="container">
+		<a href="<?php echo get_field('url',$ad->ID); ?>" target="_blank">
+			<img src="<?php echo get_field('desktop_image',$ad->ID); ?>" class="hide-mobile" style="width: 100%;">
+			<img src="<?php echo get_field('mobile_image',$ad->ID); ?>" class="show-mobile" style="width: 100%;">
+		</a>
+	</div>
 </section>
