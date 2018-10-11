452cba083c9d10717d229398ba22d1c4d50d5037
Jon Senterfitt <jonsenterfitt@Jons-MacBook-Pro.local>
2018-10-11 10:40:27 -0500

    Added more in slider

--- single.php
+++ single.php
@@ -213,7 +213,7 @@
 
 						$related_query = new WP_Query( 
 							array( 
-								'posts_per_page' => 2,
+								'posts_per_page' => 3,
 								'category__and' => $r_categories
 							)
 						);
